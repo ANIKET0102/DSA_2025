b1eb5d0bb4e3e3efcf2b8a1912de5f265a9b6cef
Aniket Pawar <anipawar9028@gmai.com>
2025-06-02 18:18:19 +0530

    first commit

---
+++ first.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int n = 5;
+    // cout<<(n>=0 ? "positive" : "negative")<<endl;
+    // return 0;
+
+    // loops
+    //  while loop
+    //  int count = 1;
+    //  int n;
+    //  cin >> n;
+    //  while (count <= n)
+    //  {
+    //      cout << count << " " ;
+    //      count += 1;
+    //  }
+
+    // for loop
+
+    // int i;
+    // int n;
+    // cin>>n;
+    // for (int i = n+1; i <= 10; i++)
+    // {
+    // cout<<i<<" ";
+    // }
+
+    // int n;
+    // int sum = 0;
+    // cin>>n;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //    sum += i;
+    // }
+    // cout<<sum;
+
+    // int n;
+    // cin>>n;
+    // int sum = 0;
+    // for (int i = 0; i <= n; i++)
+    // {
+    //     if(i%2!=0){
+    //         sum += i;
+    //     }
+    // }
+    // cout<<sum;
+
+
+
+
+
+    // int n = 7;
+    // bool isPrime = true;
+
+    // for (int i = 2; i < n - 1 ; i++)
+    // {
+    //     if (n%i == 0)
+    //     {
+    //         isPrime = false;
+    //         break;
+    //     }
+    // }
+
+    // if (isPrime == true)
+    // {
+    //     cout<<"PRIME"<<endl;
+    // }else{
+    //     cout<<"NON PRIME"<<endl;
+    // }
+    
+
+    return 0;
+}
